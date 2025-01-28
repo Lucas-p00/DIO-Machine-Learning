e51bca6f6383df4c756ba1b035712287c26be8cb
Lucas Pinheiro <lucaspinheiro19072000@gmail.com>
2025-01-28 15:24:04 -0300

    Add input para digitar frase

---
+++ Desafios/Sistema de Assistência Virtual/text_to_speech copy.py
@@ -0,0 +1,12 @@
+from gtts import gTTS
+
+def translation(text, language, arquive):
+    gtts_object = gTTS(text = text, 
+                  lang = language,
+                  slow = False)
+    gtts_object.save(f'audios/{arquive}')
+
+
+text = input("Digite sua frase em português:\n")
+
+translation(text, "pt", "audio.wav")
